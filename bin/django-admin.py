d2b8cb37b1ffaf8dd0046f3b25c36917ffc49ea5
victor-enogwe <victor.enogwe@gmail.com>
2020-01-20 08:17:51 +0100

    setup project

---
+++ bin/django-admin.py
@@ -0,0 +1,5 @@
+#!/Users/victorenogwe/Development/Work-Personal/cards-for-humanity-python-django/bin/python
+from django.core import management
+
+if __name__ == "__main__":
+    management.execute_from_command_line()
