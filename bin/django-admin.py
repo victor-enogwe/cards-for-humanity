44638851b9ea846bf29606236fc4a92dad1493d3
victor-enogwe <victor.enogwe@gmail.com>
2021-10-16 04:29:21 +0100

    update packages

--- bin/django-admin.py
+++
@@ -1,5 +0,0 @@
-#!/Users/victorenogwe/Development/Work-Personal/cards-against-humanity-python-django/bin/python
-from django.core import management
-
-if __name__ == "__main__":
-    management.execute_from_command_line()
